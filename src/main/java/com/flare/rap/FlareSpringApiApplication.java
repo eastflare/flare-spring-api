43edf8a4479e8b7f2bc8d2b5efe98fd1c81f5127
eastflare <eastflare@outlook.com>
2024-06-06 17:33:11 +0900

    first commit

---
+++ src/main/java/com/flare/rap/FlareSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.flare.rap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlareSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlareSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flare/rap/FlareSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flare.rap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlareSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
