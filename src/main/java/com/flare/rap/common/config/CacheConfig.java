3992d267954c9a27bf9c6ab5bbb4b3a5f1b9c3e4
eastflare <72382172+eastflare@users.noreply.github.com>
2024-08-06 07:21:19 +0000

    컨피그추가

---
+++ src/main/java/com/flare/rap/common/config/CacheConfig.java
@@ -0,0 +1,45 @@
+package com.flare.rap.common.config;
+
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+
+import org.ehcache.config.CacheConfiguration;
+import org.ehcache.config.builderr;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.MemoryUnit;
+import org.ehcache.jsr107.Eh107Configuration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.lgensol.eswf.message.model.MessageSimpleVO;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public CacheManager EhcacheManager() {
+
+        CacheConfiguration<String, MessageSimpleVO> cachecConfig =
+                CacheConfigurationBuilder
+                .newCacheConfigurationBuilder(String.class,
+                        MessageSimpleVO.class,
+                        ResourcePoolsBuilder.newResourcePoolsBuilder()
+                                .heap(30, MemoryUnit.MB)
+                                .offheap(40, MemoryUnit.MB)
+                                .build())
+                .withExpiry(ExpiryPolicyBuilder.noExpiration())
+                .build();
+
+        CachingProvider cachingProvider = Caching.getCachingProvider();
+        CacheManager cacheManager = cachingProvider.getCacheManager();
+
+        javax.cache.configuration.Configuration<String, MessageSimpleVO> configuration =
+                Eh107Configuration.fromEhcacheCacheConfiguration(cachecConfig);
+        cacheManager.createCache("i18nCache", configuration);
+
+        return cacheManager;
+    }
+}
