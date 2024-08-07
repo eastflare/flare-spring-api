8ce9711e961c5c00bb2bcd6ed273b5ecde583683
eastflare <72382172+eastflare@users.noreply.github.com>
2024-08-07 01:29:48 +0000

    끝날때까지 끝난게 아니다

--- src/main/java/com/flare/rap/notification/model/NotificationGroupDivisionResponseVO.java
+++ src/main/java/com/flare/rap/notification/model/NotificationGroupDivisionResponseVO.java
@@ -1,29 +1,20 @@
 package com.flare.rap.notification.model;
 
-import com.flare.rap.common.model.CrudVO;
-
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor
 @Data
-@EqualsAndHashCode(callSuper = true)
 @SuperBuilder
-public class NotificationGroupRequestVO extends CrudVO {
+@Builder
+public class NotificationGroupDivisionResponseVO {
 
-    @Schema(description = "통보처아이디", example = "1")
-    private Integer ntdkId;
+    @Schema(description = "통보처구분코드", example = "CMN")
+    private String ntdkDivsCd;
 
-    @Schema(description = "통보처제목", example = "심의 통보처")
-    private String ntdkNm;
-
-    @Schema(description = "통보처설명", example = "심의 통보처")
-    private String ntdkDesc;
+    @Schema(description = "통보처구분코드이름", example = "일반")
+    private String ntdkDivsNm;
 
 }
