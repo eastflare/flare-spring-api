d472df11e129baef70e699524d06d916bdd74155
eastflare <72382172+eastflare@users.noreply.github.com>
2024-08-07 04:22:31 +0000

    막아막아

--- src/main/java/com/flare/project/webService/WebServiceFactoryImpl.java
+++ src/main/java/com/flare/project/webService/WebServiceFactoryImpl.java
@@ -12,26 +12,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebServiceFactoryImpl implements WebServiceFactory {
 
-    private final WebService webServiceHrDepartment;
-    private final WebService webServiceHrEmployee;
-    private final WebService webServiceApprovalEntrust;
-    private final WebService webServiceApprovalType2Request;
-    private final WebService webServiceApprovalType3;
+    // private final WebService webServiceHrDepartment;
+    // private final WebService webServiceHrEmployee;
+    // private final WebService webServiceApprovalEntrust;
+    // private final WebService webServiceApprovalType2Request;
+    // private final WebService webServiceApprovalType3;
 
     @Override
     public WebService getWebService(String serviceCode){
 
-        if(InterfaceUrlKeys.HR_DEPARTMENT.equals(serviceCode)){
-            return webServiceHrDepartment;
-        }else if(InterfaceUrlKeys.HR_EMPLOYEE.equals(serviceCode)){
-            return webServiceHrEmployee;
-        }else if(InterfaceUrlKeys.APPROVAL_ENTRUST.equals(serviceCode)){
-            return webServiceApprovalEntrust;
-        }else if(InterfaceUrlKeys.APPROVAL_AUTO.equals(serviceCode)){
-            return webServiceApprovalType2Request;
-        }else if(InterfaceUrlKeys.APPROVAL_LIST.equals(serviceCode)){
-            return webServiceApprovalType3;
-        }
+        // if(InterfaceUrlKeys.HR_DEPARTMENT.equals(serviceCode)){
+        //     return webServiceHrDepartment;
+        // }else if(InterfaceUrlKeys.HR_EMPLOYEE.equals(serviceCode)){
+        //     return webServiceHrEmployee;
+        // }else if(InterfaceUrlKeys.APPROVAL_ENTRUST.equals(serviceCode)){
+        //     return webServiceApprovalEntrust;
+        // }else if(InterfaceUrlKeys.APPROVAL_AUTO.equals(serviceCode)){
+        //     return webServiceApprovalType2Request;
+        // }else if(InterfaceUrlKeys.APPROVAL_LIST.equals(serviceCode)){
+        //     return webServiceApprovalType3;
+        // }
 
         return null;
     }
