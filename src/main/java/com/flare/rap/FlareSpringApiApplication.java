50ac54401a99be59d3dd92f1a0f3d0da4f9a3996
eastflare <72382172+eastflare@users.noreply.github.com>
2024-08-07 02:58:20 +0000

    2222

--- src/main/java/com/flare/rap/FlareSpringApiApplication.java
+++ src/main/java/com/flare/rap/FlareSpringApiApplication.java
@@ -3,7 +3,7 @@ package com.flare.rap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.propertiecan;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
